6951a3a8c4be3af9277ae5b4a2268dc0fbe4ff39
Ben Kellman <kellman.ben@gmail.com>
2020-07-16 16:44:37 -0700

    Got FreeRTOS Working

--- src/main.c
+++ src/main.c
@@ -1,54 +1,50 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
+#include <libopencm3/cm3/scb.h>
+
+#include <FreeRTOS.h>
+#include <task.h>
 
 #define LED_PIN GPIO15
 
+static void TaskBlinkLED(void* pvParameters);
+
 static void gpio_setup(void)
 {
-	/* Enable GPIOD clock. */
-	/* Manually: */
-	// RCC_AHB1ENR |= RCC_AHB1ENR_IOPDEN;
-	/* Using API functions: */
-	rcc_periph_clock_enable(RCC_GPIOD);
+    /* Enable GPIOD clock. */
+    /* Manually: */
+    // RCC_AHB1ENR |= RCC_AHB1ENR_IOPDEN;
+    /* Using API functions: */
+    rcc_periph_clock_enable(RCC_GPIOD);
 
-	/* Set GPIO12 (in GPIO port D) to 'output push-pull'. */
-	/* Manually: */
-	// GPIOD_CRH = (GPIO_CNF_OUTPUT_PUSHPULL << (((8 - 8) * 4) + 2));
-	// GPIOD_CRH |= (GPIO_MODE_OUTPUT_2_MHZ << ((8 - 8) * 4));
-	/* Using API functions: */
-	gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LED_PIN);
+    /* Set GPIO12 (in GPIO port D) to 'output push-pull'. */
+    /* Manually: */
+    // GPIOD_CRH = (GPIO_CNF_OUTPUT_PUSHPULL << (((8 - 8) * 4) + 2));
+    // GPIOD_CRH |= (GPIO_MODE_OUTPUT_2_MHZ << ((8 - 8) * 4));
+    /* Using API functions: */
+    gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LED_PIN);
 }
 
 int main(void)
 {
-	int i;
+    scb_set_priority_grouping(0x300);
 
-	gpio_setup();
+    xTaskCreate(TaskBlinkLED, "LED", 256, NULL, 1, NULL);
 
-	/* Blink the LED (PC8) on the board. */
-	while (1) {
-		/* Manually: */
-		// GPIOD_BSRR = GPIO12;		/* LED off */
-		// for (i = 0; i < 1000000; i++)	/* Wait a bit. */
-		//	__asm__("nop");
-		// GPIOD_BRR = GPIO12;		/* LED on */
-		// for (i = 0; i < 1000000; i++)	/* Wait a bit. */
-		//	__asm__("nop");
+    vTaskStartScheduler();
 
-		/* Using API functions gpio_set()/gpio_clear(): */
-		// gpio_set(GPIOD, GPIO12);	/* LED off */
-		// for (i = 0; i < 1000000; i++)	/* Wait a bit. */
-		//	__asm__("nop");
-		// gpio_clear(GPIOD, GPIO12);	/* LED on */
-		// for (i = 0; i < 1000000; i++)	/* Wait a bit. */
-		//	__asm__("nop");
+    while (1) {}
+    return 0;
+}
 
-		/* Using API function gpio_toggle(): */
-		gpio_toggle(GPIOD, LED_PIN);	/* LED on/off */
-		for (i = 0; i < 1000000; i++) {	/* Wait a bit. */
-			__asm__("nop");
-		}
-	}
+static void TaskBlinkLED(void *pvParamterers)
+{
+    const TickType_t xDelay = 500 / portTICK_PERIOD_MS;
+    gpio_setup();
 
-	return 0;
+    while (1)
+    {
+        vTaskDelay(xDelay);
+        gpio_toggle(GPIOD, LED_PIN);
+    }
 }
